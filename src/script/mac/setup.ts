39c4628668a64a2207d97f4ff0837c34208398c5
abhinandvk <abhinandvk41@gmail.com>
2023-11-19 23:25:45 +0530

    updated

---
+++ src/script/mac/setup.ts
@@ -0,0 +1,59 @@
+export const setupMAcScript =`#!/bin/bash
+
+# Install Xcode Command Line Tools
+xcode-select --install
+
+# Install Homebrew (if not installed)
+if ! command -v brew &> /dev/null; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+# Install Xcode from the App Store (manual step)
+# Print message to guide the user to install Xcode from the App Store
+echo "Please open the App Store and search for Xcode. Once you find it, click on the 'Get' button to start the installation. You can also use the following link to open Xcode in the App Store:"
+open "macappstore://apps.apple.com/us/app/xcode/id497799835"
+
+# Wait for the user to install Xcode manually
+read -p "Press Enter after you have installed Xcode."
+
+# Add Homebrew's executable directory to the front of the PATH
+echo 'export PATH="/usr/local/bin:$PATH"' >> ~/.zshrc
+
+# Check if Homebrew is installed
+brew doctor
+
+# Install Node and Watchman using Homebrew
+brew install node
+brew install watchman
+
+# Tap homebrew/cask-versions and install Zulu 11
+brew tap homebrew/cask-versions
+brew install --cask zulu11
+
+# Get path to where cask was installed to double-click installer
+brew info --cask zulu11
+
+
+# Wait for the user to install Android Studio manually
+read -p "Press Enter after you have installed Android Studio and setup emulator and SDK."
+
+# Download and install Android Studio (manual step)
+# Add the following lines to ~/.zshrc
+echo 'export ANDROID_HOME=$HOME/Library/Android/sdk' >> ~/.zshrc
+echo 'export PATH=$PATH:$ANDROID_HOME/emulator' >> ~/.zshrc
+echo 'export PATH=$PATH:$ANDROID_HOME/platform-tools' >> ~/.zshrc
+
+# Install CocoaPods
+sudo gem install cocoapods
+
+
+# Check versions
+echo "Node.js version: $(node -v)"
+echo "Gem version: $(gem -v)"
+echo "Ruby version: $(ruby -v)"
+echo "Watchman version: $(watchman -v)"
+echo "Zulu JDK 11 version: $(brew info --cask zulu11 | grep Version | awk '{print $2}')"
+echo "Android Studio version: (Please check manually)"
+echo "CocoaPods version: $(pod --version)"
+echo "Xcode version: $(xcodebuild -version | grep Xcode)"
+`;
---
+++ src/setupReactNativeProject.ts
@@ -0,0 +1,35 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as os from 'os';
+import { setupMAcScript } from './script/mac/setup';
+
+export const setupDisposable = vscode.commands.registerCommand('react-native-atom-template.setupReactNativeProject', async () => {
+    try {
+        const isMac = process.platform === 'darwin';
+
+        if (isMac) {
+            // Open the terminal and guide the user through the setup steps
+
+            // Create a temporary setup script file
+            const setupScriptPath = `${os.tmpdir()}/react_native_setup.sh`;
+            const setupScriptContent = setupMAcScript
+
+            fs.writeFileSync(setupScriptPath, setupScriptContent);
+
+            // Open the integrated terminal and run the setup script
+            const terminal = vscode.window.createTerminal({
+                name: 'React Native Setup',
+                cwd: vscode.workspace.rootPath || undefined,
+            });
+
+            terminal.sendText(`bash ${setupScriptPath}`);
+
+            // Focus on the terminal after opening
+            terminal.show(true);
+        } else {
+            vscode.window.showErrorMessage('This command is only supported on macOS.');
+        }
+    } catch (error: any) {
+        vscode.window.showErrorMessage(`Error setting up React Native project: ${error.message}`);
+    }
+});
