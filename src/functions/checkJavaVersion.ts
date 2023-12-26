4857a7173e5d12f7e35da92f9474fe53b36696fa
abhinandvk <abhinandvk41@gmail.com>
2023-12-26 23:22:54 +0530

    updated

--- src/functions/checkJavaVersion.ts
+++ src/functions/checkJavaVersion.ts
@@ -6,9 +6,17 @@ export function checkJavaVersion() {
   try {
     const javaVersionOutput =
       ChildProcess.execSync("java --version").toString();
+
+    // windows java version check result
+    // const javaVersionOutput = `java --version
+    // java 11.0.13 2021-10-19 LTS
+    // Java(TM) SE Runtime Environment 18.9 (build 11.0.13+10-LTS-370)
+    // Java HotSpot(TM) 64-Bit Server VM 18.9 (build 11.0.13+10-LTS-370, mixed mode)`;
     const javaVersionMatch = javaVersionOutput.match(
-      /openjdk (\d+\.\d+(\.\d+)?) \d{4}-\d{2}-\d{2} /
+      /(\d+(\.\d+)+) \d{4}-\d{2}-\d{2}/
     );
+
+    console.log("javaversion", javaVersionMatch);
 
     if (!javaVersionMatch) {
       vscode.window.showErrorMessage("Unable to determine Java version.");
